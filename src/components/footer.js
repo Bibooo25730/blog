75551de61da7a3ea063e91bb3a9915e79a40b420
Bibooo25730 <2573018577@qq.com>
2023-06-05 22:05:33 +0800

    ooter.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,14 +4,6 @@ export default function Footer() {
   return (
     <footer className='content py-6 text-center text-sm text-neutral-500 dark:text-neutral-400 space-x-2 transition-all'>
       <span>© {year} {process.env.name}</span>
-
-      <a
-        target="_blank"
-        rel="noopener"
-        href="/feed.xml"
-      >
-        RSS
-      </a>
     </footer>
   )
 }
