84494c3abe5013089bb52186bff844bbfcb00512
Bibooo25730 <2573018577@qq.com>
2023-06-06 22:18:27 +0800

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,10 @@ export default function Footer() {
   const date = new Date();
   const year = date.getFullYear();
   return (
-    <footer className='content py-6 text-center text-sm text-neutral-500 dark:text-neutral-400 space-x-2 transition-all'>
+    <footer className='font-sans content py-6 text-center text-sm text-neutral-500 dark:text-neutral-400 space-x-5 transition-all'>
+      <a href={'https://github.com/Bibooo25730'}><i class="nes-icon github is-small"></i></a> 
       <span>© {year} {process.env.name}</span>
+      <a className="inline-block" href={"mailto: Bibooo@88.com"}> <i class="nes-icon gmail is-small"></i></a>
     </footer>
   )
 }
