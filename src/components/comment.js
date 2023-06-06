45a37b151bc7f00f4a0cd6f3a984ae8fce86611a
Bibooo25730 <2573018577@qq.com>
2023-06-06 22:18:15 +0800

    comment

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,19 +1,63 @@
 import { DisqusJS } from 'disqusjs/react';
 import 'disqusjs/react/styles/disqusjs.css';
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
 
 export default function Comment() {
   const pathname = useRouter().asPath;
+  let [theme,setTheme] = useState('');
+  console.log(pathname)
+  useEffect(()=>{
+       let getTheme = localStorage.getItem('theme');
+       setTheme(getTheme)
+  },[])
   return (
-    <>
-      <DisqusJS
-        shortname=''
-        siteName=''
-        identifier= {pathname}
-        api=''
-        apikey=''
-      />
-      <style>{`#dsqjs:focus,#dsqjs:hover{color:inherit!important}.dark #dsqjs .dsqjs-post-body { color: #eee; }.dark #dsqjs .dsqjs-no-comment { color: #ccc; }.dark #dsqjs .dsqjs-order-label { background-color: #343434; }.dark #dsqjs .dsqjs-post-list .dsqjs-post-header .dsqjs-meta { color: #ccc; }.dark #dsqjs .dsqjs-nav-tab { color: #ccc; }.dark #dsqjs .dsqjs-tab-active { color: #eee; }.dark #dsqjs .dsqjs-nav, .dark #dsqjs .dsqjs-footer-container{border-color: rgba(255,255,255,.2);}.dark #dsqjs .dsqjs-post-list .dsqjs-post-item .dsqjs-post-header .dsqjs-meta,.dark #dsqjs .dsqjs-footer{color: rgba(255,255,255,.6)}`}</style>
-    </>
+   <>
+     { theme === 'dark'?  <section class="nes-container is-dark">
+        <section class="message-list">
+          <section class="message -left">
+          
+
+            <div class="nes-balloon from-left is-dark">
+              <p>Hello 网友</p>
+            </div>
+          </section>
+
+          <section class="message -right">
+
+            <div class="nes-balloon from-right is-dark">
+              <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+            </div>
+           
+          </section>
+          
+          <i class="nes-mario "></i>
+        </section>
+      </section>: <section class="nes-container">
+        <section class="message-list">
+          <section class="message -left">
+            <i class="nes-bcrikko is-small"></i>
+
+            <div class="nes-balloon from-left">
+              <p>Hello NES.css</p>
+            </div>
+          </section>
+
+          <section class="message -right">
+
+            <div class="nes-balloon from-right">
+              <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+            </div>
+            <i class="nes-bcrikko is-small"></i>
+          </section>
+        </section>
+      </section>}  
+      <section className='text-center mt-6 mb-8'>
+      <label  for="textarea_field"></label>
+      <textarea  style={{'caretColor':'red'}}  id="textarea_field" class="nes-textarea"></textarea>  
+      <button type="button" class="nes-btn mt-6 is-primary">添加留言</button>
+      </section>
+      
+      </>
   )
 }
