33e94393c35ff21a942f75de6fba9111355345ed
Bibooo25730 <2573018577@qq.com>
2023-06-06 22:18:40 +0800

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import Footer from "./footer";
 export default function Layout({ children, home }) {
   return (
     <>
-      <header className='backdrop-blur z-50 bg-white/60 dark:bg-[#121212]/60 text-center transition-all'>
+      <header  className='font-sans backdrop-blur z-50 bg-white/60 dark:bg-[#121212]/60 text-center transition-all'>
         {home &&(
         <div className='py-6 transition-all space-y-2'>
           <h2 className='text-6xl'>
@@ -15,8 +15,8 @@ export default function Layout({ children, home }) {
         )}
       </header>
       <Nav />
-      <main className='max-w-3xl mx-auto block p-6'>{children}</main>
-      <a className='layout fixed bottom-4 right-4 border p-4 block backdrop-blur z-50 bg-white/60 dark:bg-[#121212]/60  hover:border-blue-600 hover:dark:border-blue-500 transition-all' href='#'>↑</a>
+      <main className='font-sans max-w-3xl mx-auto block p-6'>{children}</main>
+      <a className='font-sans layout fixed bottom-4 right-4 border p-4 block backdrop-blur z-50 bg-white/60 dark:bg-[#121212]/60  hover:border-blue-600 hover:dark:border-blue-500 transition-all' href='#'>↑</a>
       <Footer />
     </>
   )
