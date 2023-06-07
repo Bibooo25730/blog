929a38d7636558b00b19d26cbed902508cb6a997
Bibooo25730 <2573018577@qq.com>
2023-06-07 22:17:27 +0800

    page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/base.css';
 import '../styles/global.css';
+import { Provider } from "react-redux";
+import { stroe } from "../store/store";
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute='class' disableTransitionOnChange>
+  <Provider store={stroe}>  <ThemeProvider attribute='class' disableTransitionOnChange>
       <Component {...pageProps} />
-    </ThemeProvider>
+    </ThemeProvider></Provider>
   )
 }
