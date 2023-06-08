d40195dc2cd83ed146a5d169d23b0ba092c6154d
Bibooo25730 <2573018577@qq.com>
2023-06-08 21:31:09 +0800

    theme

--- src/service/theme.js
+++ src/service/theme.js
@@ -1,19 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { rejects } from "assert";
+import { resolve } from "path";
 
 
 const initialState = {
-    themes:'dark'
+    themes: 'dark'
 }
+async function ssrFetch() {
+    return new Promise((resolve,rejects)=>{
+        fetch('http://localhost:3000/api/commt', {
+        method: 'GET'
+    }).then(res=>{resolve(res.json())})
+    .catch(err=>{rejects(err)})
+    })
+}
+
+
+
+export const CommitQuery = createAsyncThunk(
+    "api/commit",
+    async () => {
+        return await ssrFetch()
+    }
+)
 const themeService = createSlice({
-    name:"theme",
-    initialState:initialState,
-    reducers:{
-        ToggleTheme(state,action){
-            console.log(state,action)
+    name: "theme",
+    initialState: initialState,
+    reducers: {
+        ToggleTheme(state, action) {
+            console.log(state, action)
             state.themes = action.payload
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(CommitQuery.fulfilled, (state, action) => {
+             state = action.payload
+        })
     }
 })
 
 export default themeService.reducer
-export const {ToggleTheme} = themeService.actions;
+export const { ToggleTheme } = themeService.actions;
