2386bd58707136900d0e4c8516564c9b250cdad1
Bibooo25730 <2573018577@qq.com>
2023-06-09 18:05:41 +0800

    src/pages/api/commt.js

--- src/pages/api/commt.js
+++ src/pages/api/commt.js
@@ -2,17 +2,18 @@ import { stat } from "fs";
 import clientPromise from "../../../lib/mongdb";
 
 export default async function handler(req, res) {
+
+  const {pathname} = req.query;
   const client = await clientPromise;
   const db = client.db("sample_mflix");
   switch (req.method) {
     case "POST":
-      console.log(req.body)
       let bodyObject = JSON.parse(req.body);
       let myPost = await db.collection("comment").insertOne(bodyObject);
       res.json({status:200});
       break;
     case "GET":
-      const allPosts = await db.collection("comment").find({}).toArray();
+      const allPosts = await db.collection("comment").find({"path":pathname}).toArray();
       res.json({ status: 200, data: allPosts });
       break;
   }
