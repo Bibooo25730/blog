07e27200c7f6cffd28343b9d05a55cfd349dc929
Bibooo25730 <Bibooo@88.com>
2023-07-02 20:47:16 +0800

    index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,12 @@ import Link from 'next/link';
 import Layout from '../components/layout';
 import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts';
+import {useEffect,useState} from "react";
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
+
   return {
     props: {
       allPostsData: allPostsData.slice(0, 5),
@@ -14,6 +17,10 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
+  const [pageBoolean,setPageBoolean] = useState(false)
+  useEffect(()=>{
+    setPageBoolean(true)
+  },[])
   return (
     <Layout home>
       {/* Keep the existing code here */}
@@ -40,10 +47,19 @@ export default function Home({ allPostsData }) {
             </div>
           </li>
           ))}
-          <li className='text-center text-neutral-500 dark:text-neutral-400 py-6 text-xl'>
-            <Link href={'/pages/archives'}>
-              查看所有文章
+          <li className='text-center text-neutral-500 dark:text-neutral-400  py-6 text-xs  md:text-lg'>
+            {pageBoolean?<div  className="nes-badge m-0 w-1/3 ">
+              <span className="is-dark left-0 text-lg">上一页</span>
+            </div>:<div className="nes-badge m-0 invisible w-1/3">
+              <span className="is-dark left-0 text-lg">上一页</span>
+            </div>}
+
+            <Link className='nes-badge m-0  w-1/3' href={'/pages/archives'}>
+             <span className='is-error left-0 text-lg'>所有文章</span>
             </Link>
+            <div  className="nes-badge m-0 w-1/3">
+              <span className="is-dark left-0 text-lg">下一页</span>
+            </div>
           </li>
         </ul>
     </Layout>
