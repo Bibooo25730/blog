b53f1c0f30a4b3d4265d4615b001dc4b57e0484f
Bibooo25730 <Bibooo@88.com>
2023-07-13 23:22:10 +0800

    index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,43 @@ import {useEffect,useState} from "react";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
-
+  // init
   return {
     props: {
-      allPostsData: allPostsData.slice(0, 5),
+      allPostsData: allPostsData,
     },
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({allPostsData}) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const perPage = 5;
+  const totalPages = Math.ceil(allPostsData.length / perPage);
+  const startIndex = (currentPage - 1) * perPage;
+  const endIndex = currentPage * perPage;
+  const currentPostsData = allPostsData.slice(startIndex, endIndex);
+  function handlePreviousPage() {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  function handleNextPage() {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+
+    }
+  }
+
   const [pageBoolean,setPageBoolean] = useState(false)
   useEffect(()=>{
-    setPageBoolean(true)
-  },[])
+    if(currentPage<=1){
+      setPageBoolean(false)
+    }else{
+      setPageBoolean(true)
+    }
+
+  },[currentPage])
   return (
     <Layout home>
       {/* Keep the existing code here */}
@@ -33,7 +57,7 @@ export default function Home({ allPostsData }) {
       </Head>
       {/* Add this <section> tag below the existing <section> tag */}
         <ul className='divide-y'>
-          {allPostsData.map(({ id, date, title, categories, excerpt }) => (
+          {currentPostsData.map(({ id, date, title, categories, excerpt }) => (
           <li className='py-6' key={id}>
             <Link href={`/posts/${id}`} className='block text-2xl hover:text-blue-600 dark:hover:text-blue-500 transition-all'>
               {title}
@@ -48,16 +72,14 @@ export default function Home({ allPostsData }) {
           </li>
           ))}
           <li className='text-center text-neutral-500 dark:text-neutral-400  py-6 text-xs  md:text-lg'>
-            {pageBoolean?<div  className="nes-badge m-0 w-1/3 ">
+            {pageBoolean?<div  onClick={handlePreviousPage} className="nes-badge m-0 w-1/3 ">
               <span className="is-dark left-0 text-lg">上一页</span>
-            </div>:<div className="nes-badge m-0 invisible w-1/3">
-              <span className="is-dark left-0 text-lg">上一页</span>
-            </div>}
+            </div>:''}
 
             <Link className='nes-badge m-0  w-1/3' href={'/pages/archives'}>
              <span className='is-error left-0 text-lg'>所有文章</span>
             </Link>
-            <div  className="nes-badge m-0 w-1/3">
+            <div  onClick={handleNextPage} className="nes-badge m-0 w-1/3">
               <span className="is-dark left-0 text-lg">下一页</span>
             </div>
           </li>
