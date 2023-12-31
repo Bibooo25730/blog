093489e6c7cc45591344c3d6e5bf45bb8b55d4e9
Bibooo25730 <2573018577@qq.com>
2023-12-31 23:43:33 +0800

    bl

--- src/pages/pages/links.js
+++ src/pages/pages/links.js
@@ -18,12 +18,17 @@ export default function Link() {
           {process.env.links.map(({ name, url, img, text }) => (
           <li className='group basis-1/2 md:basis-1/3 text-center my-4 p-2' key={name}>
             <a href={url} target="_blank" rel="noopener">
+
+
               <Image
                 className='group-hover:scale-110 group-hover:shadow-xl group-hover:border-blue-600 group-hover:dark:border-blue-500 group-hover:group bg-neutral-500 dark:bg-neutral-400 border-2 mx-auto transition-all'
                 src={img}
-                height={108}
-                width={108}
-                alt={name}
+ height={108}
+ width={108}
+ alt={name}
+
+            
+         
               />
               <h2 className='text-lg transition-all'>{name}</h2>
               <span className='text-neutral-500 dark:text-neutral-400 transition-all'>{text}</span>
