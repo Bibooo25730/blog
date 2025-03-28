c50bb4329d16c4ed54f3536553dd8fae6714c89d
阿林 <42339998+Bibooo25730@users.noreply.github.com>
2025-03-28 16:24:38 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className='font-sans content py-6 text-center text-sm text-neutral-500 dark:text-neutral-400 space-x-5 transition-all'>
       <a href={'https://github.com/Bibooo25730'}><i className="nes-icon github is-small"></i></a>
       <span>© {year} {process.env.name}</span>
-      <a className="inline-block" href={"http://mkd.bibooo.cn/"}> <i className="nes-icon snes is-snes"></i></a>
+      <a className="inline-block" href={"http://mkd.bibooo.cn/"}> <i className="nes-icon snes is-small"></i></a>
     </footer>
   )
 }
